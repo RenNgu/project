a2f289f2db847c13dad9932d7d46579932aea640
Ren Ngullie <ngullieren5@gmail.com>
2025-04-15 16:52:34 +0530

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
       const { data: { user } } = await supabase.auth.getUser();
       
       if (!user) {
-        toast.success('Your service request will be posted after admin reviews it');
+        toast.error('Please sign in to submit a service request');
         return;
       }
 
@@ -108,7 +108,7 @@ function App() {
       const { data: { user } } = await supabase.auth.getUser();
       
       if (!user) {
-        toast.success('Your skill offer will be posted after admin reviews it');
+        toast.error('Please sign in to submit a skill offer');
         return;
       }
 
